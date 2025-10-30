8b2e7b3dd0d8dce1fb9bfa9e73475dca5b66e866
pierre-antoine.soubrier <pierre-antoine.soubrier@epita.fr>
2025-10-30 18:38:51 +0100

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+int main(int argc, char *argv[]) {
+    if(argc ==0) {
+        std::cout << "" << std::endl;
+
+
+    }
+
+
+}
