4eb8f929b7ed2e38c0904571761593d0c4da88d5
pierre-antoine.soubrier <pierre-antoine.soubrier@epita.fr>
2025-10-31 17:03:19 +0100

    download_latest fonctionnel

--- server/API/main.py
+++ server/API/main.py
@@ -5,6 +5,9 @@ import os
 import hashlib
 app = Flask(__name__)
 
+path_to_storage = "~/Documents/DevPerso/C/ratp/server/storage/" # A changer par une variable universel 
+
+
 
 @app.route("/ratp/")
 def root():
@@ -33,20 +36,31 @@ def get_mirror_link():
 
     # Build the path of the string TODO : when creating the installer for server side make sure to modify path_to_storage
     package_name = secure_filename(package_name)
-    path_to_storage = "~/Documents/DevPerso/C/ratp/server/storage/"
-    hashedpackage=hashlib.sha512(package_name.encode('utf-8')).hexdigest()
+    path_to_storage = "/home/ashira/Documents/DevPerso/C/ratp/server/storage/"
     file_name = f"{package_name}.tar.xz"
-    path_to_latest_version = os.path.join(path_to_storage, hashedpackage, "latest_release")
+    path_to_latest_version = os.path.join(path_to_storage, package_name, "latest_release")
+    print(path_to_latest_version)
     try:
-        version = open(path_to_latest_version , "r").readline()
+        version = open(path_to_latest_version , "r").readline().split("\n")[0]
     except:
         return "Something went wrong."
     
-    path_to_file = os.path.join(path_to_storage, hashedpackage, version,file_name)
+    path_to_file = os.path.join(path_to_storage, package_name, version,file_name)
     
     return send_file(path_to_file)
 
 @app.route("/ratp/download")
 def download_specific_version():
-    # TODO : fonction that take two parameters package_name and version (normalized see notes for more info)
-    return ""
+    package_name = request.args.get("package_name")
+    version = "v" + request.args.get("version")
+
+    version =  hashlib.sha512(version.encode('utf-8')).hexdigest()
+    file_name = f"{package_name}.tar.xz"
+
+    path_to_file = os.path.join(path_to_storage, package_name, version,file_name)
+
+    return send_file(path_to_file)
+
+
+if __name__ == "__main__":
+    app.run(port=66)
