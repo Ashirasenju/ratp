b2a90e4f5c968d3eefde1287ad140be12f668e94
pierre-antoine.soubrier <pierre-antoine.soubrier@epita.fr>
2025-11-26 18:15:27 +0100

    checking version functionnal

--- local/update/update.cpp
+++ local/update/update.cpp
@@ -1,4 +1,85 @@
 #include "update.h"
+#include "curl/curl.h"
 #include <cstdio>
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <string>
 
-int update() { return 0; }
+size_t WriteCallback(void *contents, size_t size, size_t nmemb,
+                     std::string *out) {
+  out->append((char *)contents, size * nmemb);
+  return size * nmemb;
+}
+
+int extractHasToUpdate(const std::string &json) {
+  const std::string key = "has_to_update";
+
+  size_t pos = json.find(key);
+  if (pos == std::string::npos)
+    return -1;
+
+  pos = json.find(":", pos);
+  if (pos == std::string::npos)
+    return -1;
+
+  while (pos < json.size() && (json[pos] < '0' || json[pos] > '9'))
+    pos++;
+
+  if (pos >= json.size())
+    return -1;
+
+  return json[pos] - '0';
+}
+
+int isLatest(std::string package_name, std::string version) {
+  CURL *curl;
+  CURLcode res;
+  std::string response;
+  std::string url =
+      "http://127.0.0.1:66/ratp/isLatest?package_name=" + package_name +
+      "&version=" + version;
+  curl = curl_easy_init();
+  if (curl) {
+    curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+
+    res = curl_easy_perform(curl);
+    curl_easy_cleanup(curl);
+
+    if (res != CURLE_OK) {
+      std::cerr << "Curl Error :" << curl_easy_strerror(res) << std::endl;
+      return 1;
+    }
+  }
+  int return_value = extractHasToUpdate(response);
+  std::cout << return_value << std::endl;
+  if (return_value == 0 || return_value == 1) {
+    return return_value;
+  } else {
+    return -4;
+  }
+}
+
+void download(std::string package_name) {}
+int update() {
+  std::filesystem::path cur_dir = std::filesystem::current_path();
+  std::ifstream registry("registry");
+  std::string line;
+  std::string package_name = "";
+  std::string version = "";
+  while (getline(registry, line)) {
+    package_name = line.substr(0, line.find("|"));
+    version = line.substr(line.find("|") + 2, 5);
+    int status_update = isLatest(package_name, version);
+    std::cout << status_update;
+    if (status_update == -4) {
+      std::cout << package_name
+                << " couldn't be found in the distant server ..." << std::endl;
+    } else if (status_update) {
+      std::cout << package_name << " has to update" << std::endl;
+    }
+  }
+  return 0;
+}
--- server/API/main.py
+++ server/API/main.py
@@ -5,8 +5,8 @@ import os
 import hashlib
 app = Flask(__name__)
 
-path_to_storage = "~/Documents/DevPerso/C/ratp/server/storage/" # A changer par une variable universel 
-
+# A changer par une variable universel
+path_to_storage = "~/Documents/DevPerso/C/ratp/server/storage/"
 
 
 @app.route("/ratp/")
@@ -15,15 +15,23 @@ def root():
 
 
 @app.route("/ratp/isLatest")
-def update():   
-    connection = sqlite3.connect("../db/db")
+def update():
+    connection = sqlite3.connect("/home/ashira/Documents/DevPerso/C/ratp/server/db/db")
     cursor = connection.cursor()
 
     package_name = request.args.get("package_name")
     version = request.args.get("version")
-
     query = "SELECT latest_update FROM packages WHERE package_name=?"
-    latest_realease = float(cursor.execute(query, (package_name,)).fetchall()[0])
+    latest_realease = cursor.execute(
+        query, (package_name,)).fetchall()
+    print(latest_realease)
+    if latest_realease == []:
+        return jsonify({"has_to_update" : 4})
+    print(version)
+    version = version[0] + "." + version[2] + version[4]
+    latest_realease = latest_realease[0][0]
+    latest_realease = latest_realease[0] + "." + latest_realease[2] + latest_realease[4]
+    latest_realease = float(latest_realease)
     if latest_realease > float(version):
         return jsonify({"has_to_update": 1})
     else:
@@ -38,26 +46,30 @@ def get_mirror_link():
     package_name = secure_filename(package_name)
     path_to_storage = "/home/ashira/Documents/DevPerso/C/ratp/server/storage/"
     file_name = f"{package_name}.tar.xz"
-    path_to_latest_version = os.path.join(path_to_storage, package_name, "latest_release")
+    path_to_latest_version = os.path.join(
+        path_to_storage, package_name, "latest_release")
     print(path_to_latest_version)
     try:
-        version = open(path_to_latest_version , "r").readline().split("\n")[0]
+        version = open(path_to_latest_version, "r").readline().split("\n")[0]
     except:
         return "Something went wrong."
-    
-    path_to_file = os.path.join(path_to_storage, package_name, version,file_name)
-    
+
+    path_to_file = os.path.join(
+        path_to_storage, package_name, version, file_name)
+
     return send_file(path_to_file)
+
 
 @app.route("/ratp/download")
 def download_specific_version():
     package_name = request.args.get("package_name")
     version = "v" + request.args.get("version")
 
-    version =  hashlib.sha512(version.encode('utf-8')).hexdigest()
+    version = hashlib.sha512(version.encode('utf-8')).hexdigest()
     file_name = f"{package_name}.tar.xz"
 
-    path_to_file = os.path.join(path_to_storage, package_name, version,file_name)
+    path_to_file = os.path.join(
+        path_to_storage, package_name, version, file_name)
 
     return send_file(path_to_file)
 
