974411597b0363cc38d3a5825cd9b04fe01a1284
pierre-antoine.soubrier <pierre-antoine.soubrier@epita.fr>
2026-02-15 01:03:47 +0100

    minor fixes

--- local/commands/install.cpp
+++ local/commands/install.cpp
@@ -31,61 +31,8 @@ int install(char *argv[], int argc) {
 
 
 
-    download(url,package_name);
-    
-    
-    std::cout << "Decompressing " << filename << " ..." << std::endl;
-    std::string command = "tar -xvf " + destination_cache.string() + " -C " +
-                          temp_dir.string() + "/" + " > /dev/null 2>&1";
-    std::system(command.c_str());
-
-    std::filesystem::path path_to_install = temp_dir / "install.sh";
-    std::string path_to_install_ =
-        path_to_install.string() + " > /dev/null 2>&1";
-    if (std::filesystem::exists(path_to_install)) {
-      chmod(path_to_install.c_str(), 0755);
-    } else {
-      std::cerr << "❌ install.sh not found in " << package_name << "\n";
-      continue;
-    }
-    std::filesystem::path path_to_version = temp_dir / ".version";
-
-    std::cout << "Installing " << package_name << std::endl;
-    std::ifstream version(path_to_version.c_str());
-    if (!version.is_open()) {
-      std::cout << "❌Couldn't check the version of the package. Installation "
-                   "Aborted."
-                << std::endl;
-    }
-
-    int status_install = std::system(path_to_install_.c_str());
-
-    if (status_install == -1) {
-      std::cout << "❌Something went wrong with the installation of "
-                << package_name
-                << "please try to contact the editor of the package"
-                << std::endl;
-      return -1;
-    }
-    std::cout << "✅ " << package_name
-              << " installation procedure is successfull." << std::endl;
-    std::cout << "Adding " << package_name << " to the registry..."
-              << std::endl;
-    std::string version_str;
-    
-    std::getline(version, version_str);
-    std::ofstream registry("registry");
-    std::string new_package = package_name + "|" + version_str + ";";
-    registry << new_package << "\n";
-    registry.close();
-    version.close();
-    std::cout << "✅ " << package_name << " succesfully added to the registry"
-              << std::endl;
-    std::cout << "Clearing..." << std::endl;
-    std::filesystem::remove_all(temp_dir);
-    std::filesystem::create_directory(temp_dir);
-    std::cout << "✅ " << package_name
-              << " was successfully installed on your computer." << std::endl;
-  }
+  download(url,package_name);
+  install(package_name,0);
   return 0;
+}
 }
