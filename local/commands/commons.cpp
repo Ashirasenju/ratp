7fcc3f797d55e56faeb3ef4ea9ad2a001a20804f
pierre-antoine.soubrier <pierre-antoine.soubrier@epita.fr>
2026-02-15 01:02:53 +0100

    format

--- local/commands/commons.cpp
+++ local/commands/commons.cpp
@@ -1,8 +1,8 @@
 #include "commons.h"
-#include <string>
-#include <filesystem>
 #include "curl/curl.h"
 #include <cstdio>
+#include <filesystem>
+#include <string>
 #include <sys/stat.h>
 
 #include <cstdlib>
@@ -11,12 +11,11 @@
 #include <iostream>
 #include <ostream>
 #include <string>
-#include <iostream>
 
-size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userp) {
-    std::string* output = static_cast<std::string*>(userp);
-    output->append(static_cast<char*>(contents), size * nmemb);
-    return size * nmemb;
+size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp) {
+  std::string *output = static_cast<std::string *>(userp);
+  output->append(static_cast<char *>(contents), size * nmemb);
+  return size * nmemb;
 }
 int extractHasToUpdate(const std::string &json) {
   const std::string key = "has_to_update";
@@ -67,202 +66,195 @@ int isLatest(std::string package_name, std::string version) {
   }
 }
 
-int download(const std::string url,std::string package_name){
-      std::filesystem::path cur_dir = std::filesystem::current_path();
+int download(const std::string url, std::string package_name) {
+  std::filesystem::path cur_dir = std::filesystem::current_path();
 
   CURL *curl = curl_easy_init();
   if (!curl) {
     std::cerr << "Cannot init liburl.\n";
     return -1;
   }
-      std::string filename = package_name + ".tar.xz";
+  std::string filename = package_name + ".tar.xz";
 
-      FILE *fp = fopen(filename.c_str(), "wb");
-    if (!fp) {
-      std::cerr << "  ⚠️  Cannot open a new buffer for the downloaded file. '"
-                << filename << "\n";
-      return -1;
-    }
-    curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
+  FILE *fp = fopen(filename.c_str(), "wb");
+  if (!fp) {
+    std::cerr << "  ⚠️  Cannot open a new buffer for the downloaded file. '"
+              << filename << "\n";
+    return -1;
+  }
+  curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+  curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
 
-    CURLcode res = curl_easy_perform(curl);
-    fclose(fp);
+  CURLcode res = curl_easy_perform(curl);
+  fclose(fp);
 
-    if (res != CURLE_OK)
-      std::cerr << "  ❌ Curl Error '" << package_name
-                << "': " << curl_easy_strerror(res) << '\n';
-    else
-      std::cout << "✅ Download finished → " << filename << "\n";
-    std::filesystem::path downloaded_content = cur_dir / filename;
-    std::filesystem::path destination_cache = cur_dir / ".cache" / filename;
-    std::filesystem::path temp_dir = cur_dir / ".temp";
+  if (res != CURLE_OK)
+    std::cerr << "  ❌ Curl Error '" << package_name
+              << "': " << curl_easy_strerror(res) << '\n';
+  else
+    std::cout << "✅ Download finished → " << filename << "\n";
+  std::filesystem::path downloaded_content = cur_dir / filename;
+  std::filesystem::path destination_cache = cur_dir / ".cache" / filename;
+  std::filesystem::path temp_dir = cur_dir / ".temp";
 
-    auto packet_size = std::filesystem::file_size(downloaded_content);
-    if (packet_size == 0) {
-      std::cout << "The package is empty. Please contact the maintainer of the package." << std::endl;
-      return -1;
-    }
+  auto packet_size = std::filesystem::file_size(downloaded_content);
+  if (packet_size == 0) {
+    std::cout
+        << "The package is empty. Please contact the maintainer of the package."
+        << std::endl;
+    return -1;
+  }
 
+  try {
+    std::filesystem::rename(downloaded_content, destination_cache);
+    std::cout << "✅  " << filename << " was successfully added to cache."
+              << "\n";
 
-
-    try {
-      std::filesystem::rename(downloaded_content, destination_cache);
-      std::cout << "✅  " << filename << " was successfully added to cache."
-                << "\n";
-
-    } catch (const std::filesystem::filesystem_error &e) {
-      std::cout << "❌ " << ""
-                << "Couldn't be moved to .cache directory. Please check if "
-                   "the .cache directory exists."
-                << std::endl;
-    }
-    curl_easy_cleanup(curl);
-    return 0;
+  } catch (const std::filesystem::filesystem_error &e) {
+    std::cout << "❌ " << ""
+              << "Couldn't be moved to .cache directory. Please check if "
+                 "the .cache directory exists."
+              << std::endl;
+  }
+  curl_easy_cleanup(curl);
+  return 0;
 }
 
+int package_exist(const std::string url) {
+  {
 
-int package_exist(const std::string url){
-{
-   
-    CURL* curl = curl_easy_init();
+    CURL *curl = curl_easy_init();
     std::string response;
     if (curl) {
-        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, static_cast<void*>(&response));
-        curl_easy_setopt(curl, CURLOPT_NOSIGNAL, 1L);
+      curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+      curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+      curl_easy_setopt(curl, CURLOPT_WRITEDATA, static_cast<void *>(&response));
+      curl_easy_setopt(curl, CURLOPT_NOSIGNAL, 1L);
 
-        CURLcode res = curl_easy_perform(curl);
+      CURLcode res = curl_easy_perform(curl);
 
-        if (res == CURLE_OK) {
-          
+      if (res == CURLE_OK) {
 
-              if (response.find("1") != std::string::npos){
-                      curl_easy_cleanup(curl);
-                  return 1;
-                }
-              else{
-                          curl_easy_cleanup(curl);
-                  return 0;}
-            }
+        if (response.find("1") != std::string::npos) {
+          curl_easy_cleanup(curl);
+          return 1;
         } else {
-          std::cout << "somthing went wrong" << std::endl;
+          curl_easy_cleanup(curl);
+          return 0;
         }
-
-        curl_easy_cleanup(curl);
+      }
+    } else {
+      std::cout << "somthing went wrong" << std::endl;
     }
 
-    return 69;
+    curl_easy_cleanup(curl);
+  }
+
+  return 69;
 }
 
-int install(std::string package_name,int is_update){
-      std::filesystem::path cur_dir = std::filesystem::current_path();
+int install(std::string package_name, int is_update) {
+  std::filesystem::path cur_dir = std::filesystem::current_path();
 
-    std::string filename = package_name + ".tar.xz";
-    std::filesystem::path destination_cache = cur_dir / ".cache" / filename;
-    std::filesystem::path temp_dir = cur_dir / ".temp";
+  std::string filename = package_name + ".tar.xz";
+  std::filesystem::path destination_cache = cur_dir / ".cache" / filename;
+  std::filesystem::path temp_dir = cur_dir / ".temp";
 
-std::cout << "Decompressing " << filename << " ..." << std::endl;
-    std::string command = "tar -xvf " + destination_cache.string() + " -C " +
-                          temp_dir.string() + "/" + " > /dev/null 2>&1";
-    std::system(command.c_str());
+  std::cout << "Decompressing " << filename << " ..." << std::endl;
+  std::string command = "tar -xvf " + destination_cache.string() + " -C " +
+                        temp_dir.string() + "/" + " > /dev/null 2>&1";
+  std::system(command.c_str());
 
-    std::filesystem::path path_to_install = temp_dir / "install.sh";
-    std::string path_to_install_ =
-        path_to_install.string() + " > /dev/null 2>&1";
-    if (std::filesystem::exists(path_to_install)) {
-      chmod(path_to_install.c_str(), 0755);
-    } else {
-      std::cerr << "❌ install.sh not found in " << package_name << "\n";
-      return 0;
-    }
-    std::filesystem::path path_to_version = temp_dir / ".version";
+  std::filesystem::path path_to_install = temp_dir / "install.sh";
+  std::string path_to_install_ = path_to_install.string() + " > /dev/null 2>&1";
+  if (std::filesystem::exists(path_to_install)) {
+    chmod(path_to_install.c_str(), 0755);
+  } else {
+    std::cerr << "❌ install.sh not found in " << package_name << "\n";
+    return 0;
+  }
+  std::filesystem::path path_to_version = temp_dir / ".version";
 
-    std::cout << "Installing " << package_name << std::endl;
-    std::ifstream version(path_to_version.c_str());
-    if (!version.is_open()) {
-      std::cout << "❌Couldn't check the version of the package. Installation "
-                   "Aborted."
-                << std::endl;
-    }
-    std::string version_str;
-    std::getline(version,version_str);
-    version.close();
-
-
-    int status_install = std::system(path_to_install_.c_str());
-
-    if (status_install == -1) {
-      std::cout << "❌Something went wrong with the installation of "
-                << package_name
-                << "please try to contact the editor of the package"
-                << std::endl;
-      return -1;
-    }
-    std::cout << "✅ " << package_name
-              << " installation procedure is successfull." << std::endl;
-    std::cout << "Adding " << package_name << " to the registry..."
+  std::cout << "Installing " << package_name << std::endl;
+  std::ifstream version(path_to_version.c_str());
+  if (!version.is_open()) {
+    std::cout << "❌Couldn't check the version of the package. Installation "
+                 "Aborted."
               << std::endl;
+  }
+  std::string version_str;
+  std::getline(version, version_str);
+  version.close();
 
+  int status_install = std::system(path_to_install_.c_str());
 
-    
-    if(!is_update){
-            std::ofstream registry("registry");
-            std::string new_package = package_name + "|" + version_str + ";";
-            registry << new_package << "\n";
-            registry.close();
-            std::cout << "✅ " << package_name << " succesfully added to the registry"
-                      << std::endl;
-            std::cout << "Clearing..." << std::endl;
-            std::filesystem::remove_all(temp_dir);
-            std::filesystem::create_directory(temp_dir);
-            std::cout << "✅ " << package_name
-                      << " was successfully installed on your computer." << std::endl;
-            return 0;
+  if (status_install == -1) {
+    std::cout << "❌Something went wrong with the installation of "
+              << package_name
+              << "please try to contact the editor of the package" << std::endl;
+    return -1;
+  }
+  std::cout << "✅ " << package_name
+            << " installation procedure is successfull." << std::endl;
+  std::cout << "Adding " << package_name << " to the registry..." << std::endl;
 
-    } else{
-    std::string input_file = "registry"; // Fichier à traiter
-    std::string output_file = "registry.temp";    // Fichier temporaire
-        std::string line;
+  if (!is_update) {
+    std::ofstream registry("registry");
+    std::string new_package = package_name + "|" + version_str + ";";
+    registry << new_package << "\n";
+    registry.close();
+    std::cout << "✅ " << package_name << " succesfully added to the registry"
+              << std::endl;
+    std::cout << "Clearing..." << std::endl;
+    std::filesystem::remove_all(temp_dir);
+    std::filesystem::create_directory(temp_dir);
+    std::cout << "✅ " << package_name
+              << " was successfully installed on your computer." << std::endl;
+    return 0;
+
+  } else {
+    std::string input_file = "registry";       // Fichier à traiter
+    std::string output_file = "registry.temp"; // Fichier temporaire
+    std::string line;
     std::string prefix = package_name + "|";
     std::ifstream infile(input_file);
     if (!infile.is_open()) {
-        std::cerr << "Impossible d'ouvrir le fichier " << input_file << std::endl;
-        return 1;
+      std::cerr << "Impossible d'ouvrir le fichier " << input_file << std::endl;
+      return 1;
     }
 
     std::ofstream outfile(output_file);
     if (!outfile.is_open()) {
-        std::cerr << "Impossible de créer le fichier " << output_file << std::endl;
-        return 1;
+      std::cerr << "Impossible de créer le fichier " << output_file
+                << std::endl;
+      return 1;
     }
-      while (std::getline(infile, line)) {
-        if (line.rfind(prefix, 0) != 0) { // Vérifie que la ligne NE commence PAS par prefix
-            outfile << line << "\n";
-        } else{
-            outfile << package_name << "|" << version_str << ";" << "\n";
-        }
+    while (std::getline(infile, line)) {
+      if (line.rfind(prefix, 0) !=
+          0) { // Vérifie que la ligne NE commence PAS par prefix
+        outfile << line << "\n";
+      } else {
+        outfile << package_name << "|" << version_str << ";" << "\n";
+      }
     }
-        infile.close();
+    infile.close();
     outfile.close();
     if (std::remove(input_file.c_str()) != 0) {
-        std::cerr << "Erreur lors de la suppression du fichier original\n";
-        return 1;
+      std::cerr << "Erreur lors de la suppression du fichier original\n";
+      return 1;
     }
 
     if (std::rename(output_file.c_str(), input_file.c_str()) != 0) {
-        std::cerr << "Erreur lors du renommage du fichier filtré\n";
-        return 1;
+      std::cerr << "Erreur lors du renommage du fichier filtré\n";
+      return 1;
     }
-    }
-                std::cout << "✅ " << package_name << " succesfully added to the registry"
-                      << std::endl;
-            std::cout << "Clearing..." << std::endl;
-            std::filesystem::remove_all(temp_dir);
-            std::filesystem::create_directory(temp_dir);
-            std::cout << "✅ " << package_name
-                      << " was successfully installed on your computer." << std::endl;
-                      return 0;
-    
+  }
+  std::cout << "✅ " << package_name << " succesfully added to the registry"
+            << std::endl;
+  std::cout << "Clearing..." << std::endl;
+  std::filesystem::remove_all(temp_dir);
+  std::filesystem::create_directory(temp_dir);
+  std::cout << "✅ " << package_name
+            << " was successfully installed on your computer." << std::endl;
+  return 0;
 }
