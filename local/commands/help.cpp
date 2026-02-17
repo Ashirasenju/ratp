ec7ab4fe7eac9b7e7580733837c96f3c4738c315
pierre-antoine.soubrier <pierre-antoine.soubrier@epita.fr>
2026-02-17 23:17:16 +0100

    added --local to help

--- local/commands/help.cpp
+++ local/commands/help.cpp
@@ -1,4 +1,4 @@
 #include "help.h"
 std::string help() {
-    return "Usage : ratp command [options]\nCommands :\nratp install : install a package from distant webserveur\nratp update  : download all the new packages and put them into the cache\nratp purge   : delete a package";
+    return "Usage : ratp command [options]\nCommands :\nratp install : install a package from distant webserveur. Options avaliable : \n --local : install a package from local tarball. Usage : ratp install --local /path/to/file\nratp update  : download all the new packages and put them into the cache\nratp purge   : delete a package";
 }
